0104ee4c6ea75f5bc2b92fe0060d4296b0c1c853
AmrTamer23 <amrtamer23mj@gmail.com>
2024-12-04 23:20:31 +0200

    feat: compress file func

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
+use flate2::write::GzEncoder;
+use flate2::Compression;
+use std::fs::File;
+use std::io::prelude::*;
 use std::io::{self, Write};
+use std::path::Path;
 
 fn check_if_exists(file_path: &str) -> bool {
-    std::path::Path::new(file_path).exists()
+    Path::new(file_path).exists()
+}
+
+fn compress_file(file_path: &str) {
+    let mut file = File::open(&file_path).unwrap();
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer).unwrap();
+    let mut encoder = GzEncoder::new(Vec::new(), Compression::best());
+
+    encoder.write_all(&buffer).unwrap();
+    let compressed_data = encoder.finish().unwrap();
+    let compressed_file_path = format!("{}.gzip", file_path);
+    let mut compressed_file = File::create(&compressed_file_path).unwrap();
+    compressed_file.write_all(&compressed_data).unwrap();
 }
 
 fn main() {
@@ -17,5 +35,7 @@ fn main() {
         return;
     }
 
-    println!("File exists");
+    compress_file(&file_path);
+
+    println!("File compressed successfully");
 }
