8d71ad28893db16bb18bbf007c73ef95a98abed1
AmrTamer23 <amrtamer23mj@gmail.com>
2024-12-04 22:55:26 +0200

    feat: take the file path and validate it exists

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io::{self, Write};
+
+fn check_if_exists(file_path: &str) -> bool {
+    std::path::Path::new(file_path).exists()
+}
+
 fn main() {
-    println!("Hello, world!");
+    print!("Where is the file you want to compress?");
+    io::stdout().flush().unwrap();
+    let mut file_path = String::new();
+    io::stdin().read_line(&mut file_path).unwrap();
+
+    file_path = file_path.trim().to_string();
+
+    if !check_if_exists(&file_path) {
+        println!("File does not exist");
+        return;
+    }
+
+    println!("File exists");
 }
