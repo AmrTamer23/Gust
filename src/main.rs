65d06857ed1f89e2dbe4fa6db3b99ff7e53381bc
AmrTamer23 <amrtamer23mj@gmail.com>
2024-12-05 02:15:21 +0200

    feat: can pass the path directly via args

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,37 @@
 pub mod compress;
 pub mod utils;
 
-use std::io;
+use std::{env, io};
 
 use compress::{compress_file, compress_folder_as_gzip};
-
 use utils::check_if_exists;
 
 fn main() {
-    println!("Enter the folder you want to compress:");
+    let args: Vec<String> = env::args().collect();
     let mut path = String::new();
-    io::stdin().read_line(&mut path).unwrap();
-    let path = path.trim();
 
-    let path_type = check_if_exists(path);
+    if args.len() > 1 {
+        path = args[1].clone();
+    } else {
+        println!("Enter the folder or file you want to compress:");
+        io::stdin().read_line(&mut path).unwrap();
+        path = path.trim().to_string();
+    }
+
+    let path_type = check_if_exists(&path);
 
     if path_type == "false" {
-        println!("Folder does not exist.");
+        println!("The path does not exist.");
         return;
     } else if path_type == "folder" {
         let output_folder = format!("{}.tar.gz", path);
-        match compress_folder_as_gzip(path, &output_folder) {
+        match compress_folder_as_gzip(&path, &output_folder) {
             Ok(_) => println!("Folder compressed successfully as {}.", output_folder),
             Err(err) => println!("Error compressing folder: {}", err),
         }
     } else if path_type == "file" {
         let output_file = format!("{}.gzip", path);
-        match compress_file(path) {
+        match compress_file(&path) {
             Ok(_) => println!("File compressed successfully as {}", output_file),
             Err(err) => println!("Error compressing the file: {}", err),
         }
